63a8cb514843169f20082d7d05fc49be476d0323
Krishil-Jayswal <jayswalkrishil@gmail.com>
2025-10-21 22:06:53 +0530

    fix: cors error

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,21 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import authRouter from "./routes/auth";
 import userRouter from "./routes/user";
 import uploadRouter from "./routes/upload";
+import { Bindings } from "./types/generics";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
 
 const api = new Hono();
+
+app.use("*", (c, next) => {
+  return cors({
+    origin: c.env.CLIENT_URL,
+    allowHeaders: ["content-type", "authorization"],
+    allowMethods: ["GET", "POST"],
+  })(c, next);
+});
 
 app.get("/", (c) => {
   return c.text("Quill Stream API Server.");
